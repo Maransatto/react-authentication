0c4c181d2238c42b5fc10aef227b0a7e26be70b5
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-06 12:24:30 -0300

    feat: 391. Attaching Auth Tokens to Outgoing Requests

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -44,6 +44,10 @@ export async function action({ request }) {
     );
   }
 
-  // soon: manage that token
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
+
   return redirect("/");
 }
